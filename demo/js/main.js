d71fd1ec073bb919c91fc1f4f51e215ceaf8c582
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-21 11:26:41 +0200

    Add demo code

---
+++ demo/js/main.js
@@ -0,0 +1,36 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    2.8 - Activity: Your first visualization!
+*/
+
+document.getElementById("title").innerHTML = "Demo D3.js - Versione: " + d3.version;
+
+var svg = d3.select("#chart-area").append("svg")
+            .attr("width", 512)
+            .attr("height", 512);
+
+d3.json("data/buildings.json")
+  .then( (data) => {
+    data.forEach( (d) => {
+        d.height = + d.height;
+    })
+    var rects = svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y", 80)
+      .attr("x", (d, i) => {
+        return ((i+1) * 60);
+      })
+      .attr("width", 40)
+      .attr("height", (d) => {
+        return d.height;
+      })
+      .attr("fill", (d) => {
+        return "blue";
+      });
+  })
+  .catch((error) => {
+    alert(error);
+  })
