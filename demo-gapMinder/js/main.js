658618e8d6675eb09a7a6b2bd547efea892348a0
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-28 16:56:22 +0200

    Append svg and g

--- demo-gapMinder/js/main.js
+++ demo-gapMinder/js/main.js
@@ -8,6 +8,14 @@ var margin = { left:80, right:20, top:50, bottom:100 };
 var height = 600 - margin.top  W- margin.bottom, 
 var width  = 800 - margin.left - margin.right;
 
+var g = d3.select("#chart-area")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+
 d3.json("data/data.json").then(function(data){
 	console.log(data);
 })
