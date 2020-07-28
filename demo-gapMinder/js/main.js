9dc4836ce39ed1bc21cec5aedf799d9beab7c218
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-07-28 16:55:47 +0200

    Add margin, height, width

--- demo-gapMinder/js/main.js
+++ demo-gapMinder/js/main.js
@@ -4,6 +4,10 @@
 *    Project 2 - Gapminder Clone
 */
 
+var margin = { left:80, right:20, top:50, bottom:100 };
+var height = 600 - margin.top  W- margin.bottom, 
+var width  = 800 - margin.left - margin.right;
+
 d3.json("data/data.json").then(function(data){
 	console.log(data);
 })
