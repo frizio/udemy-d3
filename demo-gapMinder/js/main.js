b7303a89bece056e8efc5880ea86048ad390408d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-08-31 18:39:39 +0200

    Format tooltip content

--- demo-gapMinder/js/main.js
+++ demo-gapMinder/js/main.js
@@ -91,10 +91,10 @@ continents.forEach(function(continent, i) {
 var tip = d3.tip().attr('class', 'd3-tip')
     .html(function(d) {
         var text = "<strong>Country:</strong> <span style='color:red'>" + d.country + "</span><br>";
-        text += "<strong>Continent:</strong> <span style='color:red'>" + d.continent + "</span><br>";
-        text += "<strong>Life Expectancy:</strong> <span style='color:red'>" + d.life_exp + "</span><br>";
-        text += "<strong>GDP Per Capita:</strong> <span style='color:red'>" + d.income + "</span><br>";
-        text += "<strong>Population:</strong> <span style='color:red'>" + d.population + "</span><br>";
+        text += "<strong>Continent:</strong> <span style='color:red;text-transform:capitalize'>" + d.continent + "</span><br>";
+        text += "<strong>Life Expectancy:</strong> <span style='color:red'>" + d3.format(".2f")(d.life_exp) + "</span><br>";
+        text += "<strong>GDP Per Capita:</strong> <span style='color:red'>" + d3.format("$,.0f")(d.income) + "</span><br>";
+        text += "<strong>Population:</strong> <span style='color:red'>" + d3.format(",.0f")(d.population) + "</span><br>";
         return text;
     });
 g.call(tip);
